96ae500ef554581da1f1ad7cdc0b36e4bcf04ea5
yzhizai <dsf_ihep@yeah.net>
2020-12-25 23:10:20 +0800

    add IRI and NRI output

--- R/compare.R
+++ R/compare.R
@@ -33,14 +33,33 @@ setMethod('compare.model', signature = signature(obj1 = c('Radiomics.out'),
                    print.auc.y = 0.45)
             })
 
+            pred.1 <- ifelse(obj1@iROC$predictor > obj1@cmat$threshold[1], 0.9, 0.1)
+            pred.2 <- ifelse(obj2@iROC$predictor > obj2@cmat$threshold[1], 0.9, 0.1)
+
+
+
+
+            dt.idi.nri <- data.frame(Label = as.numeric(obj1@iROC$response)-1,
+                                     pred.1 = pred.1,
+                                     pred.2 = pred.2)
+
+
+            idi.nri.out <- capture.output(reclassification(data = dt.idi.nri,
+                                         cOutcome = 1,
+                                         predrisk1 = pred.1,
+                                         predrisk2 = pred.2,
+                                         cutoff = c(0, 0.5, 1)))
             cmat <- bind_rows(obj1@cmat, obj2@cmat) %>%
               add_column(Model = c('Obj1', 'Obj2'), .before = 1)
 
             pptx <- add_slide(pptx, layout = 'Two Content')
             ph_with(pptx, value = p.roc, location = ph_location_left())
-
-            ph_with(pptx, value = cmat,
+            ph_with(pptx, value = idi.nri.out,
                     location = ph_location_right())
+
+            pptx <- add_slide(pptx, layout = 'Title and Content')
+            ph_with(pptx, value = cmat,
+                    location = ph_location_type(type = 'body'))
 
             print(pptx, target = fpath)
           })
